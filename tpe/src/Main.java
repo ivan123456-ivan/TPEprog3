52706d6d458305094c5a1eb46964445cbf83b534
Iván Calcagno <ivancalcagno3003@gmail.com>
2024-05-17 18:23:08 -0300

    acomodando paths

--- tpe/src/Main.java
+++ tpe/src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 
 	public static void main(String args[]) {
-		Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
+		Servicios servicios = new Servicios("src\\datasets\\Procesadores.csv", "src\\datasets\\Tareas.csv");
 		
 		System.out.println(servicios.servicio1("T3"));
+
 	}
 }
