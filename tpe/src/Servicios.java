9957dd0cae131ed6e109448e588bee14c5b4250e
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2024-05-21 15:57:49 -0300

    cambiamos las linkedst por hashmap y propusimos las complejidades de los metodos

--- tpe/src/Servicios.java
+++ tpe/src/Servicios.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,8 +13,8 @@ import utils.Tarea;
  * de implementación.
  */
 public class Servicios {
-	private LinkedList<Procesador> procesadores;
-	private LinkedList<Tarea> tareas;
+	private HashMap<String, Procesador> procesadores;
+	private HashMap<String, Tarea> tareas;
 
 	/*
      * Expresar la complejidad temporal del constructor.
@@ -23,46 +24,56 @@ public class Servicios {
 		CSVReader reader = new CSVReader();
 		reader.readProcessors(pathProcesadores);
 		reader.readTasks(pathTareas);
-		this.procesadores= new LinkedList<>(reader.getProcesadores());
-		this.tareas= new LinkedList<>(reader.getTareas());
+		this.cargarProcesadores(reader.getProcesadores());
+		this.cargarTareas(reader.getTareas());
 	}
+
+	private void cargarProcesadores(List<Procesador> auxList){
+		for (Procesador procesador : auxList) {
+			this.procesadores.put(procesador.getId(), procesador);
+		}
+	}
+	private void cargarTareas(List<Tarea> auxList){
+		for (Tarea tarea : auxList) {
+			this.tareas.put(tarea.getId(), tarea);
+		}
+	}
+
 	
 	/*
      * Expresar la complejidad temporal del servicio 1.
+	 * al ser la estructura que usamos una hashmap la  complejidad del metodo es O(1)
      */
 	public Tarea servicio1(String ID) {	
-		for (Tarea t : this.tareas) {
-			if(t.getId().equals(ID)){
-				return t;
-			}
-		}
-		return null;
+		return this.tareas.get(ID);
 	}
     
     /*
      * Expresar la complejidad temporal del servicio 2.
+	 * la complejidad del metodo es O(n) siendo n la cantidad de elementos en la HashMap
      */
 	public List<Tarea> servicio2(boolean esCritica) {
-		LinkedList<Tarea> aux= new LinkedList<>();
-		for (Tarea t : this.tareas) {
-			if(t.isEs_critica()==esCritica){
-				aux.add(t);
+		LinkedList<Tarea> auxList = new LinkedList<Tarea>();
+		for (HashMap.Entry<String, Tarea> tarea : this.tareas.entrySet()) {
+			if(tarea.getValue().isEs_critica() == esCritica){
+				auxList.add(tarea.getValue());
 			}
 		}
-		return aux;
+		return auxList;
 	}
 
     /*
      * Expresar la complejidad temporal del servicio 3.
+	 * la complejidad del metodo es O(n) siendo n la cantidad de elementos en la HaskMap
      */
 	public List<Tarea> servicio3(int prioridadInferior, int prioridadSuperior) {
-		LinkedList<Tarea> aux= new LinkedList<>();
-		for (Tarea t : this.tareas) {
-			if((prioridadInferior > t.getPrioridad())&&(prioridadSuperior < t.getPrioridad())){
-				aux.add(t);
+		LinkedList<Tarea> auxList = new LinkedList<Tarea>();
+		for (HashMap.Entry<String, Tarea> tarea : this.tareas.entrySet()) {
+			if((tarea.getValue().getPrioridad() >=prioridadInferior) && (tarea.getValue().getPrioridad()>= prioridadSuperior)){
+				auxList.add(tarea.getValue());
 			}
 		}
-		return aux;
+		return auxList;
 	}
 
 }
